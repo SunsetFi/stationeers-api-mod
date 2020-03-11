2fa0cf61ebe11d32c04c2be1b244c0089025095a
RoboPhred <robophred@gmail.com>
2020-03-10 17:51:19 -0700

    Use string for referenceId

--- Payloads/ThingItem.cs
+++ Payloads/ThingItem.cs
@@ -1,16 +1,12 @@
 
 
 using Assets.Scripts.Objects;
-using UnityEngine;
 
 namespace WebAPI.Payloads
 {
     public class ThingItem
     {
-        public long referenceId { get; set; }
-
-        // For javascript usage: js does not support long values.
-        public string referenceIdStr { get; set; }
+        public string referenceId { get; set; }
 
         public string customName { get; set; }
 
@@ -18,8 +14,7 @@ namespace WebAPI.Payloads
 
         public static void CopyFromThing(ThingItem item, Thing thing)
         {
-            item.referenceId = thing.ReferenceId;
-            item.referenceIdStr = thing.ReferenceId.ToString();
+            item.referenceId = thing.ReferenceId.ToString();
             item.position = Vector3Item.FromVector3(thing.Position);
             item.customName = thing.IsCustomName ? thing.CustomName : null;
         }
