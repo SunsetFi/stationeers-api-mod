1bc0afedd997a90ebcb272ece29aa3533ef28a10
RoboPhred <robophred@gmail.com>
2020-03-22 08:07:13 -0700

    Remove steamid from login response.

--- Payloads/LoginPayload.cs
+++ Payloads/LoginPayload.cs
@@ -1,19 +1,16 @@
 
-using WebAPI.Authentication;
 
 namespace WebAPI.Payloads
 {
     public class LoginPayload
     {
-        public string steamId { get; set; }
         public string authorization { get; set; }
 
-        public static LoginPayload FromApiUser(ApiUser user, string authorization)
+        public static LoginPayload FromToken(string token)
         {
             return new LoginPayload()
             {
-                steamId = user.steamId.ToString(),
-                authorization = authorization
+                authorization = token
             };
         }
     }
