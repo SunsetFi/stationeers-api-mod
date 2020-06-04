9c7296c926ecc41fadd938b7d0de121a26bc6507
Melair <melair@caldonia.net>
2020-06-04 18:59:59 +0100

    Refactor and add spawned gas, reagents, temp limits, constructions, etc.

--- Payloads/ThingPrefabPayload.cs
+++ Payloads/ThingPrefabPayload.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using Assets.Scripts.Objects;
 using Assets.Scripts.Objects.Items;
 using Assets.Scripts.Objects.Motherboards;
 using Assets.Scripts.Objects.Pipes;
 using Assets.Scripts.Util;
+using Reagents;
 using Newtonsoft.Json;
 
 namespace WebAPI.Payloads
@@ -35,7 +37,21 @@ namespace WebAPI.Payloads
             [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
             public Nutrition nutrition { get; set; }
             [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
-            public Meltable meltable { get; set; }
+            public Meltable meltable { get; set; }           
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            private TemperatureLimits temperatureLimits { get; set; }
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            private LogicTypes logicTypes { get; set; }
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            private List<string> slots { get; set; }
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            public Dictionary<string, double> createdReagents { get; set; }
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            public Dictionary<string, double> spawnedGases { get; set; }
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            public List<String> modes { get; set; }
+            [JsonProperty(NullValueHandling=NullValueHandling.Ignore)]
+            public List<String> constructs { get; set; }
 
             public static ThingPayload FromThing(Thing prefab)
             {
@@ -58,8 +74,112 @@ namespace WebAPI.Payloads
                 {
                     item.meltable = Meltable.FromIce(ice);
                 }
+
+                if (prefab is ILogicable iLogicable)
+                {
+                    item.logicTypes = LogicTypes.FromILogicable(iLogicable);
+                }
+
+                // TODO: Paintable
+
+                item.temperatureLimits = TemperatureLimits.FromThing(prefab);
+
+                if (prefab.Slots.Count > 0)
+                {
+                    item.slots = SlotsFromThing(prefab);
+                }
+
+                if (prefab is Item thingItem)
+                {
+                    item.createdReagents = ReagentsFromItem(thingItem);
+                }
+
+                if (prefab is Ore ore) 
+                {
+                    item.spawnedGases = SpawnedGasFromOre(ore);
+                }
+
+                if (prefab.HasModeState && prefab.ModeStrings != null)
+                {
+                    item.modes = new List<string>(prefab.ModeStrings);
+                }
+                
+                if (prefab is IConstructionKit kit)
+                {
+                    item.constructs = ConstructsFromKit(kit);
+                }
                 
                 return item;
+            }
+
+            public static Dictionary<string, double> SpawnedGasFromOre(Ore ore)
+            {   
+                if (ore.SpawnContents.Count > 0)
+                {
+                    var spawnedGases = new Dictionary<string, double>();
+
+                    foreach (SpawnGas gas in ore.SpawnContents) {
+                        spawnedGases.Add(gas.Name, gas.Quantity);
+                    }
+
+
+                    return spawnedGases;
+                }
+
+                return null;
+            }
+
+            public static Dictionary<string, double> ReagentsFromItem(Item item)
+             {
+                if (item.CreatedReagentMixture.TotalReagents > 0)
+                {
+                    var createdReagents = new Dictionary<string, double>();
+
+                    foreach (Reagent reagent in Reagent.AllReagents)
+                    {
+                        var reagentQuantity = item.CreatedReagentMixture.Get(reagent);
+
+                        if (reagentQuantity > 0)
+                        {
+                            createdReagents.Add(reagent.DisplayName, reagentQuantity);
+                        }
+                    }
+
+                    return createdReagents;
+                }
+
+                return null;
+            }
+
+            public static List<string> SlotsFromThing(Thing thing) {
+                var slots = new List<string>();
+
+                for (int i = 0; i < thing.Slots.Count; i++) {
+                    var slot = thing.Slots[i];
+                    slots.Add(string.IsNullOrEmpty(slot.DisplayName) ? ("Slot " + i) : slot.DisplayName);
+                }
+
+                return slots;
+            }
+
+            public static List<string> ConstructsFromKit(IConstructionKit kit) {
+                var constructedPrefabs = kit.GetConstructedPrefabs();
+
+                if (constructedPrefabs != null) 
+                {
+                    var constructs = new List<string>();
+
+                    foreach (Thing constructed in constructedPrefabs) {
+                        if (constructed != null)
+                        {
+                            constructs.Add(constructed.PrefabName);
+                        }
+                    }
+
+                    return constructs;
+                }
+
+                return null;
             }
         }
 
@@ -83,10 +203,52 @@ namespace WebAPI.Payloads
             {
                 var meltable = new Meltable();
                 meltable.temperature = ice.MeltTemperature;
-                meltable.pressure = ice.MeltPressure;
-                
+                meltable.pressure = ice.MeltPressure * RocketMath.Thousand;
+
                 return meltable;
             }
         }
+
+        public class TemperatureLimits {
+            public float shatter { get; set; }
+            public float flashpoint { get; set; }
+            public float autoignition { get; set; }
+
+            public static TemperatureLimits FromThing(Thing thing)
+            {
+                var limits = new TemperatureLimits();
+        
+                limits.shatter = thing.ShatterTemperature;
+                limits.flashpoint = thing.FlashpointTemperature;
+                limits.autoignition = thing.AutoignitionTemperature;
+
+                return limits;
+            }
+        }
+
+        public class LogicTypes {
+            public List<string> read { get; set; }
+            public List<string> write { get; set; }
+
+            public static LogicTypes FromILogicable(ILogicable logicable) {
+                var logicTypes = new LogicTypes();
+                logicTypes.read = new List<string>();
+                logicTypes.write = new List<string>();
+
+                foreach (LogicType logicType in Enum.GetValues(typeof(LogicType)))
+                {
+                    if (logicable.CanLogicRead(logicType)) {
+                        logicTypes.read.Add(logicType.ToString());
+                    }
+
+                    if (logicable.CanLogicWrite(logicType)) {
+                        logicTypes.write.Add(logicType.ToString());
+                    }
+                }
+
+                return logicTypes;
+            }
+        }
+
     }
 }
