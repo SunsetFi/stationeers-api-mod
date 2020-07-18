edc937f043f5a48ce5c2d847eb35cea9737bf9d8
RoboPhred <robophred@gmail.com>
2020-07-18 12:22:32 -0700

    Fix save endpoint

--- Controllers/SavesController.cs
+++ Controllers/SavesController.cs
@@ -1,7 +1,9 @@
 
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Assets.Scripts.Serialization;
+using Assets.Scripts.Util;
 using Ceen;
 using WebAPI.Payloads;
 using WebAPI.Server.Attributes;
@@ -15,13 +17,18 @@ namespace WebAPI.Controllers
         [WebRouteMethod(Method = "POST")]
         public async Task PostSave(IHttpContext context, PostSavePayload body)
         {
+            if (body == null)
+            {
+                throw new BadRequestException("A body with a fileName must be specified.");
+            }
+
             if (body.fileName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
             {
                 throw new BadRequestException("Filename contains invalid characters.");
             }
 
             string worldDirectory = Path.Combine(XmlSaveLoad.CheckFiles(), "saves/" + body.fileName);
-            await Dispatcher.RunOnMainThread(() => XmlSaveLoad.Instance.ForceWriteWorld(worldDirectory));
+            UnityMainThreadDispatcher.Instance().Enqueue(XmlSaveLoad.Instance.ForceWriteWorld(worldDirectory));
             await context.SendResponse(HttpStatusCode.OK, body);
         }
     }
