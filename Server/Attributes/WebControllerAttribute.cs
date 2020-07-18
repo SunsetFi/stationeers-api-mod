2817ec0c3b151c332b0ddcf398fb611025491ba9
RoboPhred <robophred@gmail.com>
2020-07-18 12:22:43 -0700

    Add lastSave tracking

--- Server/Attributes/WebControllerAttribute.cs
+++ Server/Attributes/WebControllerAttribute.cs
@@ -2,8 +2,7 @@
 namespace WebAPI.Server.Attributes
 {
 
-    [System.AttributeUsage(System.AttributeTargets.Class)
-]
+    [System.AttributeUsage(System.AttributeTargets.Class)]
     public class WebControllerAttribute : System.Attribute
     {
         public string Path { get; set; }
